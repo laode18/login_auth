cf6225560f428947645f0d72f83e6e19857c61e1
La Ode Muhammad Fitrah <laodemuhammadfitrah18@gmail.com>
2022-07-01 22:56:22 +0700

    login commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php 
+
+include 'config.php';
+
+session_start();
+
+error_reporting(0);
+
+if (isset($_SESSION['username'])) {
+    header("Location: welcome.php");
+}
+
+if (isset($_POST['submit'])) {
+	$email = $_POST['email'];
+	$password = md5($_POST['password']);
+
+	$sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+	$result = mysqli_query($conn, $sql);
+	if ($result->num_rows > 0) {
+		$row = mysqli_fetch_assoc($result);
+		$_SESSION['username'] = $row['username'];
+		header("Location: welcome.php");
+	} else {
+		echo "<script>alert('Woops! Email or Password is Wrong.')</script>";
+	}
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+	<title>Login</title>
+</head>
+<body>
+	<div class="container" style="width : 600px;">
+		<div class="card-body p-0">
+		<form action="" method="POST" class="login-email">
+			<p class="login-text" style="font-size: 2rem; font-weight: 800;">Login</p>
+			<div class="input-group">
+				<input type="email" placeholder="Email" name="email" value="<?php echo $email; ?>" required>
+			</div>
+			<div class="input-group">
+				<input type="password" placeholder="Password" name="password" value="<?php echo $_POST['password']; ?>" required>
+			</div>
+			<div class="input-group">
+				<button name="submit" class="btn">Login</button>
+			</div>
+			<hr>
+                <a href="https://accounts.google.com/" class="btn btn-google btn-user btn-block">
+                   <i class="fab fa-google fa-fw"></i> Login with Google
+                </a>
+				<a href="https://id-id.facebook.com/" class="btn btn-facebook btn-user btn-block">
+                   <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                </a>
+                <hr>
+				<br>
+			<center><p class="login-register-text">Don't have an account? <a href="register.php">Register Here</a>.</p></center>
+		</form>
+		</div>
+	</div>
+</body>
+</html>
